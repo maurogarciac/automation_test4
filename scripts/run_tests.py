c47b007455b354e37c547997fb4e4358ce78ce9e
maurogarciac <maurogarciacoto@gmail.com>
2022-10-20 20:10:29 -0300

    added outline comments for step code later

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -5,28 +5,34 @@ import sys
 
 
 def run_tests(browser: List[str]) -> str:
-    if browser[1].lower() == "--browser" or browser[1].lower() == "-b":
-        if browser[2].lower() == "firefox":
+    cmd_option = browser[1].lower()
+    cmd_parameter = browser[2].lower()
+
+    if cmd_option == "--browser" or cmd_option == "-b":
+        if cmd_parameter == "firefox":
+            #Run the FirefoxDriver class
             return "Running tests on Firefox web browser"
-        elif browser[2].lower() == "chrome":
+        elif cmd_parameter == "chrome":
+            #Run the ChromeDriver class
             return "Running tests on Chrome web browser"
         return  """
                 Please provide a valid browser parameter.\n
                 The available browsers are:
                 [Firefox, Chrome]
                 """
-    if browser[1].lower() == "--help" or browser[1].lower() == "-h":
+    elif cmd_option == "--help" or cmd_option == "-h":
         return  """
                 Usage: run_tests.py [OPTION] [PARAMETER]
-                
+
                 -b,  --browser       allows to pass a browser as a parameter
                 -h,  --help          displays a list of all available options
 
                 Long and short forms of options are interchangeable and have no
                 effect in the funcionality of the script.
                 Parameters are not mandatory, it depends on the option chosen.
+                Options are mandatory.
                 """
     else:
-        return "Please provide a valid option, use the '--help' for more information" 
+        return "Please provide a valid option, use the '--help' option for more information"
 
 print(run_tests(sys.argv))
---
+++ steps/tests.py
@@ -0,0 +1,33 @@
+
+# 1. Go to website
+#   website opens
+
+# 2. Go to Suggestion Class Element
+#   enter Me in the box
+#   iterate through value selected until it matches mexico
+#   it matches mexico
+#   (only use xpath)
+
+# 3. Go to Dropdown Element.
+#   select option 2
+#   select option 3
+#   option 3 is selected
+#   (only use xpath)
+
+# 4. Go to Switch Window Element.
+#   wait for page to load
+#   scroll down
+#   wait for popup to appear
+#   verify if the "30 day money back guarantee" text is visible
+#   (Review the SELF PACED ONLINE TRAINING, IN DEPTH MATERIAL,
+#   LIFETIME INSTRUCTOR SUPPORT and RESUME PREPARATION text to
+#   find the bugs and report them in the RTM)
+
+# 5. Go to Switch Tab Element
+#   Click the Open tab button
+#   Scroll to the "View all courses" button
+#   Take a screenshot that includes the button and name it like the TC
+#   Return to the first tab
+#   (Only use Xpath and Css)
+
+# 6.
