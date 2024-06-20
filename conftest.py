13651e218a6df774ce19f186cff3bd844b9dbb58
Mauro Garcia <main@mgc.sh>
2024-06-19 23:29:20 -0300

    fixed issue in conftest.py

--- conftest.py
+++ conftest.py
@@ -13,4 +13,12 @@ def pytest_addoption(parser):  # Reads flags and options
 def pytest_configure(config):
     if not os.path.exists('reports'):
         os.makedirs('reports')
-    config.option.htmlpath = f"reports/report-{datetime.now().strftime('%Y-%b-%d-%H:%M')}.html"
+    config.option.htmlpath = f"reports/test-report-{datetime.now().strftime('%Y-%b-%d-%H:%M')}.html"
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):  # Reporting configuration
+    outcome = yield
+    rep = outcome.get_result()
+
+    setattr(item, "rep_" + rep.when, rep)
